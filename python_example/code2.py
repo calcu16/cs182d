487b18b09f4aca2fe80eb69e137d955f9bc83431
begerter <qwerty@Beryl-Egerters-MacBook-Pro.local>
2013-02-10 19:48:18 -0800

    adding a second easy code question

---
+++ python_example/code2.py
@@ -0,0 +1,23 @@
+
+def function50(i, L):
+    return L[i+2]
+
+def function37(L):
+    return L[-1]+L
+
+def function52(i):
+    return function4() * i
+
+def function1(j, k):
+    return (j + k) * function52(1)
+
+def function4():
+    return 3
+
+def function188(L):
+    return function37(L)+function50(2, L)
+
+def function0():
+    return function188([1,2,3,4,5,6,7,8,9])[function1(0,1)]
+
+function0()
